466cbb102a984b7d35eb579f929e7129a0c2bd84
Vkaisang41 <kaisang.vincent@student.moringaschool.com>
2025-06-24 01:05:24 +0300

    Attach variables to global object for CodeGrade compatibility

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var currentUser = 'Grace Hopper';
+global.currentUser = currentUser;
 
 var welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+global.welcomeMessage = welcomeMessage;
 
 var excitedWelcomeMessage = welcomeMessage.toUpperCase();
+global.excitedWelcomeMessage = excitedWelcomeMessage;
 
 var shortGreeting = 'Welcome, ' + currentUser[0] + '!';
+global.shortGreeting = shortGreeting;
