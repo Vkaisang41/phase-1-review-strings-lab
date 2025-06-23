d2cb81a4ff3e21a341cce1a3ea685405838bbcd4
Vkaisang41 <kaisang.vincent@student.moringaschool.com>
2025-06-24 00:24:12 +0300

    Use global variables for CodeGrade compatibility

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 var currentUser = "Grace Hopper";
+
 var welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
 var excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
 var shortGreeting = "Welcome, " + currentUser[0] + "!";
 
-module.exports = {
-  currentUser,
-  welcomeMessage,
-  excitedWelcomeMessage,
-  shortGreeting
-};
